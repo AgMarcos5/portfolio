be943f057edffa83221f5ff99f6d40df8fd843df
Marcos Aguero <aguermar@gmail.com>
2022-01-07 12:43:00 -0300

    correccion src archivos

--- js/Sprite.js
+++ js/Sprite.js
@@ -15,7 +15,7 @@ class Sprite{
         this.shadow = new Image();
         this.useShadow = true;
         if(this.useShadow){
-            this.shadow.src = "img/characters/shadow.png"
+            this.shadow.src = "../img/characters/shadow.png"
         }
         this.shadow.onload = () => {
             this.isShadowLoaded = true;
